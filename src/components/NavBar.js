d29448cce308a5f11a35e00a10bf953fa1b9b601
Bradley Mubenga <bradleymubenga@BMP.local>
2022-07-21 01:41:38 +0200

    Issues #5, #7 & #1 Fixed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,10 +5,16 @@ import logo from '../images/logo.png';
 import '../styles/NavBar.css';
 //React Router DOM
 import { NavLink } from 'react-router-dom';
+//React Icons
+import { IconContext } from "react-icons";
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { IoExitSharp } from 'react-icons/io5';
+//Animate.css
+import 'animate.css';
 
 export default function NavBar() 
 {
-    //React state used to show and hide top section of navbar when scrolling.
+    //React state used to manipulate transparency of navbar when scrolling.
     const [showNav, setShowNav] = useState(true);
     const displayNavbar = () => {
         if (window.scrollY > 10)
@@ -27,15 +33,27 @@ export default function NavBar()
             window.removeEventListener('scroll', displayNavbar);
         }
     }, []);
+    
+    //Responsive Menu Toggle Event Listener
+    const [showResponsiveMenu, setResponsiveMenu] = useState(false);
+    function navToggleShow(e)
+    {
+        e.preventDefault()
+        setResponsiveMenu(true)
+    }
+    function navToggleHide(e)
+    {
+        setResponsiveMenu(false)
+    }
 
   return (
     <header id='navBar'>
         <div className={ showNav ? 'navContainerTransparent' : 'navContainer' }>
-            <div className='navTopSection' >
-                <img src={logo} alt='studiocreative.co.za-logo' width={225}/>
+            <div className='navLeftSection' >
+                <img src={logo} alt='studiocreative.co.za-logo' className="navLogo"/>
             </div>
 
-            <div className='navBottomSection'> 
+            <div className='navRightSection'> 
                 <div className='navListWrapper'>
                     <ul className='navList'>
                         <li>
@@ -48,6 +66,59 @@ export default function NavBar()
                             <NavLink to="/contact">Contact</NavLink>
                         </li>
                     </ul>
+                </div>
+            </div>
+
+            {/*RESPONSIVE NAVIGATION*/}
+            <div className={showResponsiveMenu ? 'navResponsive navResponsiveVisible' : 'navResponsive'}>
+                <div className='navResponsiveListWrapper'>
+                    <ul className='navResponsiveList'>
+                        <li>
+                            <NavLink to="/" onClick={navToggleHide}>About</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/portfolio" onClick={navToggleHide}>Portfolio</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/contact" onClick={navToggleHide}>Contact</NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            
+            <div className='navResponsiveRightSection'>
+                <IconContext.Provider 
+                    value=
+                    {
+                        { 
+                            color: "#B8BCB3", 
+                            size: 30, 
+                            className: showResponsiveMenu ? "navHamburger hide" : "navHamburger", 
+                            attr: "hamburger menu icon" 
+                        }
+                    }
+                >
+                    <div onClick={navToggleShow}>
+                        <GiHamburgerMenu />
+                    </div>
+                </IconContext.Provider>
+                
+                <div className='navExitWrapper'>
+                    <IconContext.Provider 
+                        value=
+                        {
+                            { 
+                                color: "#B8BCB3", 
+                                size: 30, 
+                                className: showResponsiveMenu ? "navExitMenu animate__animated animate__slideInRight" : "navExitMenu hide", 
+                                attr: "exit menu icon" 
+                            }
+                        }
+                    >
+                        <div onClick={navToggleHide}>
+                            <IoExitSharp/>
+                        </div>
+                    </IconContext.Provider>
                 </div>
             </div>
         </div>
