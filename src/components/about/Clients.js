6a1c65f610421d379b8e71dc2e5be792b9e981d7
Bradley Mubenga <bradleymubenga@BMP.local>
2022-07-22 23:43:04 +0200

    Clients Logo Section Is Complete, Just Need To Make Images And Text Responsive

--- src/components/about/Clients.js
+++ src/components/about/Clients.js
@@ -1,13 +1,29 @@
 import React from 'react';
 //CSS
+import '../../styles/about/Clients.css';
 //Media Assets
+import quiverLogo from "../../images/quiver-logo.png";
+import freedomChurchLogo from "../../images/freedom-church-logo.png";
+import vivaGymLogo from '../../images/viva-gym-logo.png';
 
 export default function Clients() {
     //PUT IMAGES IN AN ARRAY AND LOOP THROUGH AND MAKE A LOGO JSX FOR EACH
+    let Images = [ quiverLogo, freedomChurchLogo, vivaGymLogo ]
   return (
     <section id='clientsSection' className='clientsSection'>
+        <div>
+            <p>Recommended by our clients.</p>
+        </div>
         <div className='clientsContainer'>
-
+            {
+                Images.map((source, index) => {
+                    return (
+                        <div className='clientLogoDiv' key={index}>
+                            <img src={source} alt="client's logo" width={"150px"} />
+                        </div>
+                    )
+                })
+            }
         </div>
     </section>
   )
--- src/pages/About.js
+++ src/pages/About.js
@@ -2,12 +2,14 @@ import React from 'react';
 //Components
 import Hero from '../components/about/Hero';
 import Explore from '../components/about/Explore';
+import Clients from '../components/about/Clients';
 
 export default function About() {
   return (
     <main>
       <Hero />
       <Explore />
+      <Clients />
     </main>
   )
 }
